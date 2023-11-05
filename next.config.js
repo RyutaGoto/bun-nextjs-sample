69a470877fe75a2009fd6ed8699e07c550b1fef4
RyutaGoto <kasa8gasa2rs6@gmail.com>
2023-11-05 19:36:41 +0900

    initial commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+// const nextConfig = {}
+
+// module.exports = nextConfig
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA({
+  //next.js config
+  reactStrictMode: true,
+});
